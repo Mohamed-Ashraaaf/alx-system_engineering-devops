ee2efe212926aef058608befdeea785af6fb79bc
Mohamed_Ashraaaf <mohamed.ashraaf@gmail.com>
2023-08-09 03:44:08 -0700

    some modification

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,21 +1,5 @@
 # Puppet manifest to troubleshoot and fix Apache 500 error using strace
-
-exec { 'strace_apache':
-  command     => 'strace -o /tmp/strace_output.txt -f -s 200 -p $(pgrep apache2)',
-  path        => ['/usr/bin', '/bin'],
-  refreshonly => true,
-}
-
-exec { 'fix_apache_issue':
-  command     => '/usr/sbin/service apache2 restart',
-  path        => ['/usr/bin', '/bin'],
-  refreshonly => true,
-  subscribe   => Exec['strace_apache'],
-  onlyif      => 'test -s /tmp/strace_output.txt',
-}
-
-service { 'apache2':
-  ensure => 'running',
-  enable => true,
-  subscribe => Exec['fix_apache_issue'],
+exec {'replace':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
