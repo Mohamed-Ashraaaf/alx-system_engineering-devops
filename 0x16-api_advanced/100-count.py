8d2220a4aedd6bd9cbf8d1ee27cf7246e1d121ba
Mohamed_Ashraaaf <mohamed.ashraaf@gmail.com>
2023-08-10 11:49:12 -0700

    edits

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -6,45 +6,46 @@ import requests
 import sys
 
 
-def count_words(subreddit, word_list, word_count=None, after=None):
-    """Queries the Reddit API, parses titles, and prints keyword counts."""
+def count_words(subreddit, word_list, after=None, word_count=None):
+    if after is None:
+        after = ""
     if word_count is None:
         word_count = {}
 
-    base_url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'My User Agent'}
-    params = {'after': after}
+    url =
+    "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit, after)
+    headers =
+    {"User-Agent": "python:com.example.myredditapp:v1.0.0 (by /u/username)"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    response = requests.get(
-            base_url, headers=headers, params=params, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json()['data']
-        children = data['children']
-
-        for child in children:
-            title = child['data']['title'].lower()
-            words = title.split()
-            for word in word_list:
-                if word.lower() in words:
-                    if word in word_count:
-                        word_count[word] += 1
-                    else:
-                        word_count[word] = 1
-
-        if data['after'] is not None:
-            return count_words(
-                    subreddit, word_list, word_count, after=data['after'])
-        else:
-            sorted_words = sorted(
-                    word_count.items(), key=lambda x: (-x[1], x[0]))
-            for word, count in sorted_words:
-                print("{}: {}".format(word, count))
-    else:
+    if response.status_code != 200:
         return
 
+    data = response.json()["data"]
+    children = data["children"]
 
-if __name__ == '__main__':
+    for post in children:
+        title = post["data"]["title"].lower()
+        words = title.split()
+        for word in word_list:
+            if word.lower() in words:
+                if word in word_count:
+                    word_count[word] += 1
+                else:
+                    word_count[word] = 1
+
+    next_page = data["after"]
+    if next_page is not None:
+        count_words(
+                subreddit, word_list, after=next_page, word_count=word_count)
+    else:
+        sorted_words = sorted(
+                word_count.items(), key=lambda x: (-x[1], x[0]))
+        for word, count in sorted_words:
+            print("{}: {}".format(word, count))
+
+
+if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
         print("Ex: {} programmin 'python java javascript'".format(sys.argv[0]))
